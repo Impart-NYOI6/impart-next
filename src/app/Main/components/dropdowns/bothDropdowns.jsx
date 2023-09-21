d2dc9a0c7a78f148cfffe7ddc651c348dda9d716
arjunsaurus <arjunsaurus@users.noreply.github.com>
2023-09-21 11:51:39 -0400

    main sort functioning

---
+++ src/app/Main/components/dropdowns/bothDropdowns.jsx
@@ -0,0 +1,90 @@
+import * as React from 'react';
+import Chip from '@mui/material/Chip';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+
+export default function Tags(props) {
+    const setCats = (event, value) => {
+        const categories = props.categories(value);
+    }
+    const setInds = (event, value) => {
+        const industries = props.industries(value);
+    }
+
+  return (
+    <Stack spacing={3} sx={{ width: 250 }}>
+      <Autocomplete
+        multiple
+        id="tags-outlined"
+        options={categories}
+        getOptionLabel={(option) => option}
+        onChange={setCats}
+        // defaultValue={['Categories']}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            // variant="standard"
+            label="Categories"
+            placeholder="Categories"
+          />
+        )}
+      />
+      <Autocomplete
+        multiple
+        id="tags-outlined"
+        options={industries}
+        getOptionLabel={(option) => option}
+        onChange={setInds}
+        // defaultValue={['Industries']}
+        filterSelectedOptions
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            // variant="standard"
+            label="Industries"
+            placeholder="Industries"
+          />
+        )}
+      />      
+    </Stack>
+  );
+}
+
+const categories = ['General',
+'Career Development',
+'Skill Enhancement',
+'Problem Solving',
+'Networking',
+'Goal Setting',
+'Work-Life Balance',
+'Job Search and Interviewing',
+'Company/Industry Insight',
+'Self-Improvement and Personal Growth',
+'Ethical and Moral Questions']
+
+const industries = ['Education',
+'Construction',
+'Design',
+'Corporate Services',
+'Retail',
+'Energy & Mining',
+'Manufacturing',
+'Finance',
+'Arts',
+'Health Care',
+'Hardware & Networking',
+'Real Estate',
+'Legal',
+'Consumer Goods',
+'Agriculture',
+'Media & Communications',
+'Nonprofit',
+'Software & IT Services',
+'Transportation & Logistics',
+'Entertainment',
+'Wellness & Fitness',
+'Public Safety',
+'Public Administration'];
+
+
--- src/app/Main/page.jsx
+++ src/app/Main/page.jsx
@@ -8,43 +8,94 @@ import ReactPlayer from "react-player";
 import Comment from './components/Comment';
 // import fetch from 'node-fetch';
 // Modal.setAppElement(el);
-import Categories from './components/dropdowns/categories'
-import Industries from './components/dropdowns/industries'
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
 import { styled } from '@mui/material/styles';
+import Dropdown from './components/dropdowns/bothDropdowns'
 
 
 
 export default function() {
-    const [categories, setCategories] = useState([]);
     const [videos, setVideos] = useState([]);
     // const [videoComments, setVideoComments] = useState([])
     const [modalIsOpen, setModalIsOpen] = useState(false);
     const [selectedMovie, setSelectedMovie] = useState(null);
     const [commentArray, setCommentArray] = useState([])
     const [selectedMovieID, setSelectedMovieID] = useState(null);
+    const [categories, setCategories] = useState([]);
+    const [industries, setIndustries] = useState([]);
 
     useEffect(() => {
-        const getVideos = async () => {
-            const videoArray = [];
-            
-            const { data, error } = await supabase
-            .from('Posts')
-            .select('*');
-            data?.forEach((obj) => {
-                if (obj.video_hosted_url) {
-                  // console.log('obj.post_id: ', obj.post_id)
-                    videoArray.push({url: obj.video_hosted_url, id: obj.id, postID: obj.post_id})
-                }
-            })
-            // console.log(videoArray)
-            setVideos(videoArray)
+      const getVideos = async () => {
+        const videoArray = [];
 
+        const body = {
+          industries: industries,
+          categories: categories
+        };
+
+        if (categories.length === 0 && industries.length === 0) {
+          const response = await fetch('https://impart-server.onrender.com/posts');
+
+          const data = await response.json();
+
+          console.log('data: ', data);
+
+          data?.forEach((obj) => {
+            if (obj.video_hosted_url) {
+  
+                videoArray.push({url: obj.video_hosted_url, id: obj.id, postID: obj.post_id})
+            }
+          })
+          setVideos(videoArray)
+        } else {
+          const response = await fetch('https://impart-server.onrender.com/getPostsByCriteria', {
+            method: 'post',
+            body: JSON.stringify(body),
+            headers: {'Content-Type': 'application/json'}
+          });
+  
+          const data = await response.json();
+
+          data?.forEach((obj) => {
+            if (obj.video_hosted_url) {
+              // console.log('obj.post_id: ', obj.post_id)
+                videoArray.push({url: obj.video_hosted_url, id: obj.id, postID: obj.post_id})
+            }
+          })
+  
+          setVideos(videoArray)
         }
-        getVideos();
-    }, [categories])
+       
+        
+        
+      };
+      
+      getVideos();
+        // const getVideos = async () => {
+        //       // /getPostsByCriteria
+        //       // {
+        //       //     "industries" : ["Software & IT Services","Retail"],
+        //       //     "categories" : ["Career Development"]
+        //       // }
+        //     const videoArray = [];
+            
+        //     const { data, error } = await supabase
+        //     .from('Posts')
+        //     .select('*');
+        //     data?.forEach((obj) => {
+        //         if (obj.video_hosted_url) {
+        //           // console.log('obj.post_id: ', obj.post_id)
+        //             videoArray.push({url: obj.video_hosted_url, id: obj.id, postID: obj.post_id})
+        //         }
+        //     })
+        //     // console.log(videoArray)
+        //     setVideos(videoArray)
+
+        // }
+        // getVideos();
+    }, [categories, industries])
 
     useEffect(() => {
 
@@ -91,8 +142,7 @@ export default function() {
             <div>Impartables</div>
             <div className="flex justify-between w-full">
                 <div className="flex-col ml-4">
-                    <Categories/>
-                    <Industries/>
+                    <Dropdown inds={industries} cats={categories} categories={(catArr)=>{setCategories(catArr)}} industries={(indArr)=>{setIndustries(indArr)}}/>
                 </div>
                 <div className="border-4 border-blue-500 rounded mx-auto space-y-5">
                     {videos.map((video)=>{return <FeedItem video={video} play={(url)=>{handleWatchNow(url)}} returnID={(id)=>{setSelectedMovieID(id)}} />})}
